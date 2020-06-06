4401ecd2d7a449c8e0be5eda861294edfddfbe89
haiphamthanh <pthai076@gmail.com>
2020-06-06 22:41:25 +0700

    xml using with unmarshal

--- hello.go
+++ hello.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
+type Post struct {
+	XMLName  xml.Name  `xml:"post"`
+	Id       string    `xml:"id,attr"`
+	Content  string    `xml:"content"`
+	Author   Author    `xml:"author"`
+	Xml      string    `xml:",innerxml"`
+	Comments []Comment `xml:"comments>comment"`
+}
+
+type Author struct {
+	Id   string `xml:"id,attr"`
+	Name string `xml:",chardata"`
+}
+
+type Comment struct {
+	Id      string `xml:"id,attr"`
+	Content string `xml:"content"`
+	Author  Author `xml:"author"`
+}
+
+func main() {
+	xmlFile, err := os.Open("./xml/post.xml")
+	if err != nil {
+		fmt.Println("Error opening XML file:", err)
+		return
+	}
+	defer xmlFile.Close()
+
+	xmlData, err := ioutil.ReadAll(xmlFile)
+	if err != nil {
+		fmt.Println("Error reading XML data:", err)
+		return
+	}
+
+	var post Post
+	xml.Unmarshal(xmlData, &post)
+	fmt.Println(post)
 }
