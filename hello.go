bf1935e67303133b44d12a3c7658ccfd7128cb4f
haiphamthanh <pthai076@gmail.com>
2020-06-18 17:16:37 +0700

    [webservice] add post removing

--- hello.go
+++ hello.go
@@ -119,5 +119,23 @@ func handlePut(w http.ResponseWriter, r *http.Request) (err error) {
 }
 
 func handleDelete(w http.ResponseWriter, r *http.Request) (err error) {
+	// Retrieve item by id
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		return
+	}
+
+	post, err := retrieve(id)
+	if err != nil {
+		return
+	}
+
+	// Delete item
+	err = post.delete()
+	if err != nil {
+		return
+	}
+
+	w.WriteHeader(200)
 	return
 }
